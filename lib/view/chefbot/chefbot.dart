234885f225aa9480e9517db13ff8446cb483e483
cbasguti <sebastian.gutierrezj@udea.edu.co>
2024-03-28 09:15:26 -0500

    ajustes chefbot

--- lib/view/chefbot/chefbot.dart
+++ lib/view/chefbot/chefbot.dart
@@ -1,3 +1,4 @@
+import 'package:dart_openai/dart_openai.dart';
 import 'package:flutter/material.dart';
 
 class ChefBot extends StatefulWidget {
@@ -8,6 +9,61 @@ class ChefBot extends StatefulWidget {
 }
 
 class ChefBotState extends State<ChefBot> {
+  TextEditingController _textEditingController = TextEditingController();
+  List<Widget> _messageBubbles = [];
+  @override
+  void initState() {
+    super.initState();
+    OpenAI.apiKey = "ENTER-API-KEY-HERE";
+  }
+
+  Future<void> sendMessage(String message) async {
+    // ignore: prefer_const_declarations
+    final systemMessage = const OpenAIChatCompletionChoiceMessageModel(
+      role: OpenAIChatMessageRole.assistant,
+      content: 'talk like a chef',
+    );
+
+    final userMessage = OpenAIChatCompletionChoiceMessageModel(
+      role: OpenAIChatMessageRole.user,
+      content: message,
+    );
+
+    final requestMessages = [
+      systemMessage,
+      userMessage,
+    ];
+    try {
+      // Enviar solicitud al modelo de OpenAI para obtener respuesta
+      final response = await OpenAI.instance.chat
+          .create(model: 'gpt-3.5-turbo', messages: requestMessages);
+      setState(() {
+        _messageBubbles.add(
+          _buildMessageBubble(response.choices.first.message.content, false),
+        );
+      });
+    } catch (e) {
+      print("Error al enviar mensaje al bot: $e");
+    }
+  }
+
+  Widget _buildMessageBubble(String message, bool isUser) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+      padding: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: isUser ? Colors.blueAccent : Colors.grey[200],
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      child: Text(
+        message,
+        style: TextStyle(
+          color: isUser ? Colors.white : Colors.black,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,50 +79,51 @@ class ChefBotState extends State<ChefBot> {
           },
         ),
       ),
-      body: Container(
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                child: Image.asset(
-                  'assets/image/chefbot/chefbot.png', // Que no sea estatica
-                  width: 300,
-                  height: 300,
-                  fit: BoxFit.contain,
-                ),
-              ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              reverse: true,
+              padding: EdgeInsets.symmetric(vertical: 10.0),
+              children: _messageBubbles.reversed.toList(),
             ),
-            Container(
-              margin:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-              padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              height: 40,
-              decoration: BoxDecoration(
-                color: Colors.grey[200],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                children: [
-                  const Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "¿Qué vamos a preparar hoy?",
-                        border: InputBorder.none,
-                      ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            height: 40,
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textEditingController,
+                    decoration: InputDecoration(
+                      hintText: "¿Qué vamos a preparar hoy?",
+                      border: InputBorder.none,
                     ),
                   ),
-                  IconButton(
-                    icon: Icon(Icons.send),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.send),
+                  onPressed: () {
+                    String message = _textEditingController.text.trim();
+                    if (message.isNotEmpty) {
+                      setState(() {
+                        _messageBubbles.add(_buildMessageBubble(message, true));
+                      });
+                      sendMessage(message);
+                      _textEditingController.clear();
+                    }
+                  },
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
